b4ddc8191e5713cb0cde88d9bf27f32f0379693f
Florent Cayré <florent@commown.fr>
2018-11-29 11:05:58 +0100

    [FIX] Replace erroneous balance transaction list call by a retrieval for manual payouts
    As stated at https://stripe.com/docs/api/balance/balance_history?lang=python#balance_history-payout this call is not allowed for non-automatic payouts, or you get an error message like: "Request req_xxx: Balance transaction history can only be filtered on automatic transfers, not manual."

--- account_move_stripe_import/models/account_journal.py
+++ account_move_stripe_import/models/account_journal.py
@@ -79,8 +79,12 @@ class StripeParser(AccountMoveImportParser):
                 continue
             self.move_ref = payout['id']
             self.move_date = date.fromtimestamp(payout['arrival_date'])
-            self.result_row_list = stripe.BalanceTransaction.list(
-                payout=payout['id'], api_key=api_key, limit=1000)['data']
+            if payout['automatic'] is False:
+                self.result_row_list = [stripe.BalanceTransaction.retrieve(
+                    payout['balance_transaction'], api_key=api_key)]
+            else:
+                self.result_row_list = stripe.BalanceTransaction.list(
+                    payout=payout['id'], api_key=api_key, limit=1000)['data']
             fee_vals = defaultdict(float)
             for line in self.result_row_list:
                 for fee in line['fee_details']:
